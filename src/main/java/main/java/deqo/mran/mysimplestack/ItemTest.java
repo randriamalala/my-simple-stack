b39111a57d30efa1d4f6c7854f127b33c3316bc9
randriamalala <randriamalal@gmail.com>
2018-11-05 10:55:27 +0100

    Feat add and remove elements from the stack and Test

---
+++ src/main/java/main/java/deqo/mran/mysimplestack/ItemTest.java
@@ -0,0 +1,32 @@
+package main.java.deqo.mran.mysimplestack;
+
+import deqo.mran.mysimplestack.Item;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    Item item;
+
+    @Before
+    public void setUp() {
+        // code executé avant chaque test
+        item = new Item(new Object());
+    }
+
+    @Test
+    public void getValue() {
+        item.setValue(null);
+        Assert.assertNull(item.getValue());
+    }
+
+    @org.junit.Test
+    public void setValue() {
+        Object o = new Object();
+        item.setValue(o);
+        Assert.assertEquals(item.getValue(), o);
+    }
+}
